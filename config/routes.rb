5f9be64ed33f03581a7a2e1c4fa6573deb1e6fb8
Vishay Bhatia <vishay@addictivegifts.com>
2015-05-12 20:59:13 +0400

    Added books scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :books
   devise_for :users
   root 'pages#home'
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
