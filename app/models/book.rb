2112d1666a306944c33c1963ad66a38c20454901
Vishay Bhatia <vishay@addictivegifts.com>
2015-05-18 19:07:18 +0400

    Added sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ActiveRecord::Base
 	friendly_id :name, use: :slugged
 
 	belongs_to :user 
+	has_many :sales
 	has_attached_file :image
 	has_attached_file :resource
 
@@ -16,5 +17,8 @@ message: "Only pdfs allowed"
  
 validates :image, attachment_presence: true
 validates :resource, attachment_presence: true
+
+validates_numericality_of :price,
+greater_than: 49, message: "Must be at least 50 cents"
 
 end
